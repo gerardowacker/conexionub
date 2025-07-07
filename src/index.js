1ccf99a43880b9e1098cd20a77c1b6567417806f
Gerardo Wacker <ge@rar.vg>
2025-07-07 02:29:16 -0300

    Added a project base

---
+++ src/index.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './styles/index.css'
+import {AppError} from './views/error'
+import {RouterProvider} from './router/router';
+import {Route} from './router/route';
+import Home from "./views/home";
+import {SessionProvider} from "./components/session.context";
+import ResourceView from "./views/resource.view";
+import LoginView from "./views/login.view";
+
+export const WebRoutes = [
+    {
+        path: "/",
+        component: <Home/>
+    },
+    {
+        path: "/resource/:id",
+        component: <ResourceView/>,
+    },
+    {
+        path: "/login",
+        component: <LoginView/>
+    },
+    {
+        path: "",
+        component: <AppError></AppError>,
+        status: 404
+    },
+]
+
+const AppRoutes = () => <RouterProvider>
+    {WebRoutes.map((route, k) => <Route key={k} status={route.status ? route.status : 200}
+                                        path={route.path}>{route.component}</Route>)}
+</RouterProvider>
+
+ReactDOM.render(
+    <SessionProvider>
+        <AppRoutes/>
+    </SessionProvider>,
+    document.getElementById("root")
+)
+
+export default AppRoutes;
