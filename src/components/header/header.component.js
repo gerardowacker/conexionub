613f9ef8c24d508e14a1e0f86ac421646d255d1b
Gerardo Wacker <gerardo@wacker.com.ar>
2025-09-11 16:06:45 -0300

    Componentised `Header`

--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -1,51 +1,34 @@
-import React, { useEffect, useRef, useState } from "react";
-import "../../styles/header.css";
+import React from "react";
+import "./header.component.css";
+import Link from "../../router/link";
+import logo from '../../assets/UBLogo.png'
 
-export default function Header({ variant = "landing", breadcrumb = [] }) {
-  const isLanding = variant === "landing";
+export default class Header extends React.Component
+{
+    constructor(props)
+    {
+        super(props);
+    }
 
-  const Menu = () => (
-    <nav className="ub-nav" aria-label="Menú principal">
-      <a href="/">Inicio</a>
-      <a href="/carreras">Carreras</a>
-      <a href="/contacto">Contacto</a>
-      <a href="/universidad">Universidad</a>
-      <a href="/repositorio">Repositorio</a>
-      <a href="/tesisytrabajos">Tesis y Trabajos de Investigacion</a>
-    </nav>
-  );
+    render()
+    {
+        return (
+            <header className="ub-landing-header">
+                <div className="ub-landing-inner">
 
-  return (
-    <header className={`ub-header ${isLanding ? "landing" : "app"}`}>
-      <div className="ub-header-bar">
-        <div className="ub-header-inner">
-          <div className="ub-logo" href="/" aria-label="Universidad de Belgrano">
-            <img src="UBLogo.png" alt="UB" />
-          </div>
-
-          <Menu />
-        </div>
-      </div>
-
-      {/* Breadcrumb solo si viene data */}
-      {breadcrumb?.length > 0 && (
-        <div className="ub-breadcrumb-wrap" aria-label="Ruta de navegación">
-          <ol className="ub-breadcrumb">
-            {breadcrumb.map((item, i) => {
-              const last = i === breadcrumb.length - 1;
-              return (
-                <li key={i} className={last ? "current" : ""}>
-                  {last ? (
-                    <span>{item.label}</span>
-                  ) : (
-                    <a href={item.href}>{item.label}</a>
-                  )}
-                </li>
-              );
-            })}
-          </ol>
-        </div>
-      )}
-    </header>
-  );
+                    <nav className="ub-landing-nav">
+                        <Link to={"/"} style={{display: 'flex'}}>
+                            <img className={'header-logo'} src={logo} alt={'Logo de la Universidad de Belgrano'}/>
+                        </Link>
+                        <Link to="/">Inicio</Link>
+                        <a href="/carreras">Carreras</a>
+                        <a href="/repositorio">Repositorio</a>
+                        <a href="/universidad">Universidad</a>
+                        <a href="/tesisytrabajos">Tesis</a>
+                        <a href="/contacto">Contacto</a>
+                    </nav>
+                </div>
+            </header>
+        );
+    }
 }
