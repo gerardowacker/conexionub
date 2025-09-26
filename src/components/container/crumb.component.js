0c38cdeeb5ec7fdba2772d6619d319d2e1a0f5ee
Gerardo Wacker <gerardo@wacker.com.ar>
2025-09-26 10:47:40 -0300

    Added `key` parameters into crumb's elements

--- src/components/container/crumb.component.js
+++ src/components/container/crumb.component.js
@@ -7,19 +7,19 @@ export default class CrumbComponent extends React.Component
         return items.map((item, key) =>
         {
             if (key !== items.length - 1) return (
-                <>
+                <div style={{display: 'inherit'}} key={key}>
                     <span className={'crumb-item'}>{item}</span>
                     <span className={'crumb-item'}><b>&gt;</b></span>
-                </>
+                </div>
             )
-            else return <span className={'crumb-item'}>{item}</span>
+            else return <span key={key} className={'crumb-item'}>{item}</span>
         })
     }
 
     render()
     {
         return <div className="container-head">
-            <span className="crumb-inline">
+            <span key={'crumb'} className="crumb-inline">
                 {this.renderItems(this.props.items)}
             </span>
         </div>
