bd9a14153fb8674e0bd5d6f644ed459c46b691e0
Gerardo Wacker <gerardo@wacker.com.ar>
2025-10-13 13:18:04 -0300

    Separated external links to the right in `Header`

--- conexionub/src/components/menu/headermenu/Header.tsx
+++ conexionub/src/components/menu/headermenu/Header.tsx
@@ -10,16 +10,20 @@ export default function Header() {
             <div className={styles['ub-landing-inner']}>
 
                 <nav className={styles['ub-landing-nav']}>
-                    <Link href={"/"} style={{display: 'flex'}}>
-                        <Image className={styles['header-logo']} src={'/UBLogo.png'} height={40} width={50}
-                               alt={'Logo de la Universidad de Belgrano'} priority/>
-                    </Link>
-                    <Link href="/">Inicio</Link>
-                    <Link href="/#carreras">Carreras</Link>
-                    <Link href="/repositorio">Repositorio Ciencias y Tecnologías</Link>
-                    <Link href="https://www.ub.edu.ar">Universidad</Link>
-                    <Link href="https://repositorio.ub.edu.ar">Repositorio universitario</Link>
-                    <Link href="/contacto">Contacto</Link>
+                    <div className={styles.leftLinks}>
+                        <Link href={"/"} style={{display: 'flex'}}>
+                            <Image className={styles['header-logo']} src={'/UBLogo.png'} height={40} width={50}
+                                   alt={'Logo de la Universidad de Belgrano'} priority/>
+                        </Link>
+                        <Link href="/">Inicio</Link>
+                        <Link href="/#carreras">Carreras</Link>
+                        <Link href="/repositorio">Repositorio</Link>
+                        <Link href="/contacto">Contacto</Link>
+                    </div>
+                    <div className={styles.rightLinks}>
+                        <Link href="https://www.ub.edu.ar">Universidad ↗</Link>
+                        <Link href="https://repositorio.ub.edu.ar">Repositorio universitario ↗</Link>
+                    </div>
                 </nav>
             </div>
         </header>
