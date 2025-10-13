b5e83f4797e99ccc3940c32b14ce14164c28af37
Gerardo Wacker <gerardo@wacker.com.ar>
2025-10-13 13:59:26 -0300

    Added optional `selected` parameter in `PillMenu`

--- conexionub/src/components/menu/pillmenu/PillMenu.tsx
+++ conexionub/src/components/menu/pillmenu/PillMenu.tsx
@@ -5,15 +5,15 @@ import Link from "next/link";
 type PillMenuItem = {
     name: string,
     route: string,
-    selected: boolean
+    selected?: boolean
 }
 
 export default function PillMenu({items}: { items: PillMenuItem[] }) {
     return <ul className={styles['pill-menu']}>
         {items.map((item, key) => (
-            <Link className={styles['pill-menu-item'] + (item.selected ? ' ' + styles['pill-selected'] : '')}
+            <Link key={key} className={styles['pill-menu-item'] + (item.selected ? ' ' + styles['pill-selected'] : '')}
                   href={'repositorio' + (item.route !== '' ? '/' + item.route : '')}>
-                <li key={key}>{item.name}</li>
+                <li>{item.name}</li>
             </Link>
         ))}
     </ul>
