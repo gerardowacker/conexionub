e3b5981e742b4a08b86125c78b773c0290f01d65
Gerardo Wacker <gerardo@wacker.com.ar>
2025-10-13 21:09:33 -0300

    Replaces `img` with Next.js' `Image` element in homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,17 +6,18 @@ import Container from "@/components/container/Container";
 import styles from './page.module.css'
 import {getDegrees} from "@/app/carreras/utils";
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Home() {
     return (
         <>
             <Banner>
-                <img src={'/LogoBlancoTransparente.webp'} alt="UB" className={styles['hero-logo']}/>
+                <Image src={'/LogoBlancoTransparente.webp'} alt="UB" className={styles['hero-logo']}/>
                 <h1 id="hero-title" className={styles['hero-title']}>
                     Portal de Ciencias y Tecnologías
                 </h1>
             </Banner>
-            <Container id={'carreras'} crumb={['Inicio', <Link href={'/#carreras'}>Carreras</Link>]}>
+            <Container id={'carreras'} crumb={['Inicio', <Link key={'Carreras'} href={'/#carreras'}>Carreras</Link>]}>
                 <div className={styles['cards-2']}>
                     {getDegrees().map((degree, key) => (
                         <CareerCard key={key} title={degree.metadata.title} years={degree.metadata.years + " años"}
