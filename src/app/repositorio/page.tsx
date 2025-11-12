e995125f6b344fa8f27b4d106c4d12ab45a8c335
Gerardo Wacker <gerardo@wacker.com.ar>
2025-11-12 03:02:57 -0300

    Added a proper `ResourceList` component into the repository's landing page

--- src/app/repositorio/page.tsx
+++ src/app/repositorio/page.tsx
@@ -4,39 +4,28 @@ import Link from "next/link";
 import {get} from "@/utils/request";
 import styles from './page.module.css'
 import {Resource} from '@/types/resources'
+import ResourceList from '@/components/list/ResourceList'
 
 export default async function Repositorio() {
     const request = await get('/resources?pageSize=5&desc=true')
     const resources = Array.isArray(request.response.data?.resources) ? request.response.data.resources as Resource[] : [];
     const hasMore = !!request.response.data?.hasMore;
+    const lastResource = request.response.data?.lastResource ?? null;
 
     return (
         <>
             <Container id={'ultimas'} crumb={['Repositorio',
                 <Link key={'Últimas adiciones'} href={'#ultimas'}>Últimas adiciones</Link>]}>
                 <h1 className={styles['title']}>Últimas adiciones</h1>
-                {resources.length === 0 && <p>No hay recursos disponibles.</p>}
-                {resources.map(resource => {
-                    const titleEs = Array.isArray(resource.dc.title) ? resource.dc.title.find(t => t.language === 'es')?.title : resource.dc.title;
-                    const authors = resource.dc.contributor?.author?.length ? resource.dc.contributor.author.join(', ') : resource.dc.creator;
-                    const issuedDate = resource.dc.date?.issued ? new Date(resource.dc.date.issued) : undefined;
-                    let fecha = '';
-                    if (issuedDate) {
-                        const dia = issuedDate.getDate().toString().padStart(2, '0');
-                        const mes = (issuedDate.getMonth() + 1).toString().padStart(2, '0');
-                        const anio = issuedDate.getFullYear();
-                        fecha = `${dia}/${mes}/${anio}`;
-                    }
-                    const descriptionEs = Array.isArray(resource.dc.description) ? resource.dc.description?.find(d => d.language === 'es')?.abstract : resource.dc.description;
-                    return (
-                        <Link href={`/repositorio/recurso/${resource._id}`} className={styles['resource']}
-                              key={resource._id}>
-                            <h2 className={styles['resource-title']}>{titleEs ?? 'Sin título'}</h2>
-                            <p className={styles['resource-author']}>{authors ?? 'Sin autor'}{fecha ? `  (${fecha})` : ''}</p>
-                            <p className={styles['resource-description']}>{descriptionEs ?? 'Sin descripción disponible.'}</p>
-                        </Link>
-                    )
-                })}
+
+                <ResourceList
+                    initialResources={resources}
+                    initialHasMore={hasMore}
+                    initialLastResource={lastResource}
+                    baseEndpoint={'/resources'}
+                    initialQuery={'?pageSize=5&desc=true'}
+                />
+
                 {hasMore && (
                     <Link href={'/repositorio'} className={styles['see-more']}>
                         Ver más
